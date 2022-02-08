d68afef2ce1d7d7d664f14359c866a865d772a15
Carlos González <karlinhos@gmail.com>
2022-02-08 22:14:55 +0100

    Main version adapted to spanish

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -1,5 +1,12 @@
 import { Cell } from '../grid/Cell'
 import { BaseModal } from './BaseModal'
+import {
+  HOW_TO_PLAY,
+  HELP_FIRST_BLOCK,
+  HELP_SECOND_BLOCK,
+  HELP_THIRD_BLOCK,
+  HELP_FOURTH_BLOCK,
+} from '../../constants/strings'
 
 type Props = {
   isOpen: boolean
@@ -8,43 +15,42 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title={HOW_TO_PLAY} isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        {HELP_FIRST_BLOCK}
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
-        <Cell value="E" />
+        <Cell value="G" status="correct" />
         <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell value="L" status="present" />
-        <Cell value="O" />
         <Cell value="T" />
+        <Cell value="O" />
+        <Cell value="S" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        {HELP_SECOND_BLOCK}
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell value="V" />
+        <Cell value="O" />
+        <Cell value="C" status="present" />
         <Cell value="A" />
-        <Cell value="G" />
-        <Cell value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="L" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        {HELP_THIRD_BLOCK}
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="C" />
+        <Cell value="A" />
+        <Cell value="N" />
+        <Cell value="T" status="absent" />
+        <Cell value="O" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        {HELP_FOURTH_BLOCK}
       </p>
     </BaseModal>
   )
